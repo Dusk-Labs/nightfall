fd6b9fb7dcc5a644013f4e65602953529941c784
Eray Chumak <eray_chumak@protonmail.com>
2021-02-20 17:37:39 +0000

    Add initial video player component

---
+++ video-player/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import VideoPlayer from "./VideoPlayer/Index";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <VideoPlayer/>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
