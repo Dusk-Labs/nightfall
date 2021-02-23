b8a3a408c167a3ba850b1fe12392990dc9fed11b
Eray Chumak <eray_chumak@protonmail.com>
2021-02-23 13:10:25 +0000

    Remove fetching segment manually

--- video-player/src/VideoPlayer/Controls/SeekBar.js
+++ video-player/src/VideoPlayer/Controls/SeekBar.js
@@ -1,11 +1,10 @@
-import { MediaPlayer } from "dashjs";
 import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { VideoPlayerContext } from "../Context";
 
 import "./SeekBar.scss";
 
 function VideoControls() {
-  const { setPlayer, video, player, duration, id, offset, currentTime, setOffset, buffer } = useContext(VideoPlayerContext);
+  const { player, duration, offset, currentTime, setOffset, buffer } = useContext(VideoPlayerContext);
   const [ seeking, setSeeking ] = useState(false);
 
   const seekBarCurrent = useRef(null);
@@ -35,31 +34,17 @@ function VideoControls() {
     const rect = e.target.getBoundingClientRect();
     const percent = (e.clientX - rect.left) / rect.width;
     const videoDuration = player.duration();
-    const newTime = percent * videoDuration;
+    const newTime = Math.floor(percent * videoDuration);
     const newSegment = Math.floor(newTime / 5);
 
-    console.log(`[Seek] fetching ${newSegment}.m4s`);
+    player.attachSource(`http://localhost:8000/manifest.mpd?start_num=${newSegment}`);
 
-    const req = await fetch(`http://localhost:8000/chunks/${id}/${newSegment}.m4s`);
+    console.log(`[Seek] update offset to ${newTime} seconds (${newTime / 60} mins)`);
 
-    if (req.status === 200) {
-      player.destroy();
-      console.log(`[Seek] creating new player ${newSegment}.m4s`);
-
-      const newPlayer = MediaPlayer().create();
-
-      newPlayer.initialize(video.current, `http://localhost:8000/manifest.mpd?start_num=${Math.floor(newSegment)}`, true);
-
-      console.log(`[Seek] update offset to ${Math.round(newTime)} seconds (${Math.round(newTime) / 60} mins)`);
-
-      setOffset(Math.round(newTime));
-      setSeeking(false);
-      setPlayer(newPlayer);
-      console.log(`[Seek] playing now from ${newSegment}.m4s`);
-      newPlayer.seek(newTime);
-      newPlayer.play();
-    }
-  }, [id, player, seeking, setOffset, setPlayer, video]);
+    setOffset(oldOffset => Math.round(oldOffset - newTime));
+    setSeeking(false);
+    player.play();
+  }, [player, seeking, setOffset]);
 
   return (
     <div className="seekBar" onClick={onSeek}>
