eb67053b2d9f61e7e1b53451125b9c80fd8bcecc
Valerian G <valerian.garleanu@pm.me>
2021-03-21 08:29:27 -0700

    Using GetExitCodeProcess to check whether a process is dead instead of WaitForSingleObject as WaitForSingleObject caused UB

--- src/utils.rs
+++ src/utils.rs
@@ -31,13 +31,21 @@ cfg_if::cfg_if! {
         use ntapi::ntpsapi::NtResumeProcess;
 
         use winapi::um::winnt::PROCESS_ALL_ACCESS;
+        use winapi::um::winnt::PROCESS_QUERY_INFORMATION;
         use winapi::um::processthreadsapi::OpenProcess;
-        use winapi::um::synchapi::WaitForSingleObject;
+        use winapi::um::processthreadsapi::GetExitCodeProcess;
         use winapi::shared::winerror::WAIT_TIMEOUT;
+        use winapi::um::minwinbase::STILL_ACTIVE;
+        use winapi::shared::ntdef::NULL;
 
         pub fn pause_proc(pid: i32) {
             unsafe {
                 let process_handle = OpenProcess(PROCESS_ALL_ACCESS, 0, pid as u32);
+                
+                if process_handle == NULL {
+                    return;
+                }
+
                 NtSuspendProcess(process_handle);
             }
         }
@@ -45,14 +53,30 @@ cfg_if::cfg_if! {
         pub fn cont_proc(pid: i32) {
             unsafe {
                 let process_handle = OpenProcess(PROCESS_ALL_ACCESS, 0, pid as u32);
+                
+                if process_handle == NULL {
+                    return;
+                }
+
                 NtResumeProcess(process_handle);
             }
         }
 
         pub fn is_process_effectively_dead(pid: u32) -> bool {
             unsafe {
-                let process_handle = OpenProcess(PROCESS_ALL_ACCESS, 0, pid as u32);
-                WaitForSingleObject(process_handle, 0) != WAIT_TIMEOUT
+                let process_handle = OpenProcess(PROCESS_QUERY_INFORMATION, 0, pid as u32);
+                let mut exit_code = 0;
+
+                // process probably doesnt exist at this point
+                if process_handle == NULL {
+                    return true;
+                }
+
+                if GetExitCodeProcess(process_handle, &mut exit_code) == 0 {
+                    return true;
+                }
+
+                exit_code != STILL_ACTIVE
             }
         }
     }
